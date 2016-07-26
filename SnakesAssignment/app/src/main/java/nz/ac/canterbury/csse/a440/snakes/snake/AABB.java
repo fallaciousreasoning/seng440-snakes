e9856227b5361807d5ffd4858685277ee08abd49
Jay Harris <jay.harris@outlook.co.nz>
2016-07-26 17:57:21 +1200

    Adds a few handy methods for determining size/shape of the AABB

--- SnakesAssignment/app/src/main/java/nz/ac/canterbury/csse/a440/snakes/snake/AABB.java
+++ SnakesAssignment/app/src/main/java/nz/ac/canterbury/csse/a440/snakes/snake/AABB.java
@@ -6,4 +6,54 @@ package nz.ac.canterbury.csse.a440.snakes.snake;
 public class AABB {
     private Vector3 min;
     private Vector3 max;
+
+    /**
+     * Gets the width of the AABB
+     * @return the width of the AABB
+     */
+    public float getWidth() {
+        return max.getX() - min.getX();
+    }
+
+    /**
+     * gets the height of the bounding box
+     * @return The height of the bounding box
+     */
+    public float getHeight() {
+        return max.getY() - min.getY();
+    }
+
+    /**
+     * Gets the depth of the bounding box
+     * @return The depth of the bounding box
+     */
+    public float getDepth() {
+        return max.getZ() - min.getZ();
+    }
+
+    /**
+     * Gets the centre of the bounding box
+     * @return The centre of the bounding box
+     */
+    public Vector3 getCentre() {
+        return min
+                .add(max)
+                .div(2);
+    }
+
+    /**
+     * Gets the minimum coordinate of the bounding box
+     * @return The minimum coordinate
+     */
+    public Vector3 getMin() {
+        return min;
+    }
+
+    /**
+     * Gets the maximum coordinate of the bounding box
+     * @return the maximum coordinate of the bounding box
+     */
+    public Vector3 getMax() {
+        return max;
+    }
 }

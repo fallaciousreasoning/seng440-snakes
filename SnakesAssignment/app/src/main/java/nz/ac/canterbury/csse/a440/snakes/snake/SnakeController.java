0388b6c8777b79b3b5a9d6b2b031defb82de2439
Jay Harris <jay.harris@outlook.co.nz>
2016-07-27 17:31:07 +1200

    Updates the documentation on the snake controller

--- SnakesAssignment/app/src/main/java/nz/ac/canterbury/csse/a440/snakes/snake/SnakeController.java
+++ SnakesAssignment/app/src/main/java/nz/ac/canterbury/csse/a440/snakes/snake/SnakeController.java
@@ -4,5 +4,10 @@ package nz.ac.canterbury.csse.a440.snakes.snake;
  * An interface for controlling the snake.
  */
 public interface SnakeController {
+    /**
+     * The direction the snake should move next time the game is stepped. The snake will only move
+     * in this direction if 'step' is called on the game.
+     * @return The direction the snake should move in
+     */
     Direction getDirection();
 }

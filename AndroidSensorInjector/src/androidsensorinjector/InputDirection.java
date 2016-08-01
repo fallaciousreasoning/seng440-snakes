db42998a917719e19ed549276a24a775dc6717b3
Jay Harris <jay.harris@outlook.co.nz>
2016-08-01 12:28:27 +1200

    Documents the InputDirection class

--- AndroidSensorInjector/src/androidsensorinjector/InputDirection.java
+++ AndroidSensorInjector/src/androidsensorinjector/InputDirection.java
@@ -8,15 +8,28 @@ public class InputDirection {
     private final InputMethod method;
 
 
+    /**
+     * Creates a new input direction class
+     * @param method The input method to inject
+     * @param direction THe direction to inject
+     */
     public InputDirection(InputMethod method, Direction direction) {
         this.direction = direction;
         this.method = method;
     }
 
+    /**
+     * Gets the direction
+     * @return The direction
+     */
     public Direction getDirection() {
         return direction;
     }
 
+    /**
+     * Gets the input method
+     * @return The input method
+     */
     public InputMethod getMethod() {
         return method;
     }

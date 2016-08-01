64eaf56b6310ffda68b9fb445225b7d11353fedc
Jay Harris <jay.harris@outlook.co.nz>
2016-08-01 12:27:39 +1200

    Documents the SensorInfo class

--- AndroidSensorInjector/src/androidsensorinjector/SensorInfo.java
+++ AndroidSensorInjector/src/androidsensorinjector/SensorInfo.java
@@ -7,18 +7,34 @@ public class SensorInfo {
     private int id;
     private String name;
 
+    /**
+     * Gets the id of the sensor
+     * @return The id of the sensor
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Sets the id of the sensor
+     * @param id The id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Gets the name of the sensor
+     * @return The name of the sensor
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the sensor
+     * @param name The name of the sensor
+     */
     public void setName(String name) {
         this.name = name;
     }

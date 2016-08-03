da83df48f57a45d79a77620c8f143c0c7769d7b7
Jay Harris <jay.harris@outlook.co.nz>
2016-08-04 10:33:47 +1200

    Stubs an open gl renderer for the snake game.

---
+++ SnakesAssignment/app/src/main/java/nz/ac/canterbury/csse/a440/snakes/snake/SnakeOpenGLView.java
@@ -0,0 +1,11 @@
+package nz.ac.canterbury.csse.a440.snakes.snake;
+
+/**
+ * An open gl renderer for the snake game
+ */
+public class SnakeOpenGLView implements Renderer {
+    @Override
+    public void render(SnakeGame game) {
+
+    }
+}
